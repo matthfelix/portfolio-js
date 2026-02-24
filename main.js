f27cb68c6b34d052a07cd5c6f02947ce06f0396a
Matheus Felix <matheusfelix0996@gmail.com>
2026-02-24 18:40:38 -0300

    Inclusão novo projeto

--- main.js
+++ main.js
@@ -6,3 +6,25 @@ document.addEventListener('DOMContentLoaded', () => {
     }, index * 150);
   });
 });
+
+// Seleciona todos os cards
+const cards = document.querySelectorAll('.card');
+
+// Função para verificar se o card está visível
+function mostrarCards() {
+  const triggerBottom = window.innerHeight * 0.85;
+
+  cards.forEach(card => {
+    const cardTop = card.getBoundingClientRect().top;
+
+    if(cardTop < triggerBottom) {
+      card.classList.add('show');
+    }
+  });
+}
+
+// Evento de scroll
+window.addEventListener('scroll', mostrarCards);
+
+// Inicializa para já mostrar cards que estão visíveis ao carregar
+mostrarCards();
